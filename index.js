fa49058f3bdbe75c13b6cb70b73b1052180ef400
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-24 00:36:10 +0600

    pagenation code impliemented "

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT|| 5000;
+const cors = require('cors');  // for localy connect at different port 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();  // for .evn file create and update database password
+const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware 
@@ -9,24 +11,52 @@ app.use(express.json());
 
 //database connection 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8rorn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("emaJohn").collection("product");
-  // perform actions on the collection object
-  console.log('mongoDB in connected');
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        app.get('/product', async (req, res) => {
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {} // eita ami khujbo...
+            const cursor = productCollection.find(query);
+            let products;
+            if (page || size) {
+                //0 --> skip : 0 get 0-10:
+                //1 --> skip : 1*10 get 11-20:
+                //2 --> skip : 2*10 get 21-30:
+                //3 --> skip : 3*10 get 31-40:
+                products = await cursor.skip(page*size).limit(size).toArray();
+            } else {
+                products = await cursor.toArray();
+            }
+            res.send(products);
+        });
+
+        
+        app.get('/productCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+    }
+    finally { }
+}
+
+run().catch(console.dir);
 
 
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('jhon is running and waitting for ema');
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('port is running on', port);
 })
 
- 
+
